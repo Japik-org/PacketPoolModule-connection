30fa1dfd0cbe24a3eca13d81c5a0a8c4c22c2405
pro100kryto <pro100kryto.original@gmail.com>
2021-03-15 02:12:44 -0300

    Replace istack with jetbrains annotations

--- src/main/java/com/pro100kryto/server/modules/packetpool/connection/IPacketPoolModuleConnection.java
+++ src/main/java/com/pro100kryto/server/modules/packetpool/connection/IPacketPoolModuleConnection.java
@@ -2,7 +2,7 @@ package com.pro100kryto.server.modules.packetpool.connection;
 
 import com.pro100kryto.server.module.IModuleConnection;
 import com.pro100kryto.server.utils.datagram.packets.IPacketInProcess;
-import com.sun.istack.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 public interface IPacketPoolModuleConnection extends IModuleConnection {
     @Nullable
