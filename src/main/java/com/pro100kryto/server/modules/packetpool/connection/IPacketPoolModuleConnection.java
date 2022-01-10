052e6998bb405281461dfdd2bb241ece37933b3e
pro100kryto <pro100kryto.original@gmail.com>
2022-01-09 23:15:05 -0300

    Refactoring

--- src/main/java/com/pro100kryto/server/modules/packetpool/connection/IPacketPoolModuleConnection.java
+++ src/main/java/com/pro100kryto/server/modules/packetpool/connection/IPacketPoolModuleConnection.java
@@ -1,13 +1,13 @@
 package com.pro100kryto.server.modules.packetpool.connection;
 
 import com.pro100kryto.server.module.IModuleConnection;
-import com.pro100kryto.server.utils.datagram.packet.Packet;
+import com.pro100kryto.server.utils.datagram.packet.DatagramPacketWrapper;
 import com.pro100kryto.server.utils.datagram.pool.PoolEmptyException;
 import org.jetbrains.annotations.Nullable;
 
 public interface IPacketPoolModuleConnection extends IModuleConnection {
     @Nullable
-    Packet getNextPacket() throws PoolEmptyException;
+    DatagramPacketWrapper getNextPacket() throws PoolEmptyException;
     int getMaxCapacity();
     int getRemainingCapacity();
 }
