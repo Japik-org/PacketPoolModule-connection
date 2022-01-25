31669934a592cc7a653120b0403f633a803d117d
pro100kryto <pro100kryto.original@gmail.com>
2022-01-25 13:18:24 -0300

    Server 0.4.0 adaptation

--- src/main/java/com/pro100kryto/server/modules/packetpool/connection/IPacketPoolModuleConnection.java
+++ src/main/java/com/pro100kryto/server/modules/packetpool/connection/IPacketPoolModuleConnection.java
@@ -5,9 +5,11 @@ import com.pro100kryto.server.utils.datagram.packet.DatagramPacketWrapper;
 import com.pro100kryto.server.utils.datagram.pool.PoolEmptyException;
 import org.jetbrains.annotations.Nullable;
 
+import java.rmi.RemoteException;
+
 public interface IPacketPoolModuleConnection extends IModuleConnection {
     @Nullable
-    DatagramPacketWrapper getNextPacket() throws PoolEmptyException;
-    int getMaxCapacity();
-    int getRemainingCapacity();
+    DatagramPacketWrapper getNextPacket() throws RemoteException, PoolEmptyException;
+    int getMaxCapacity() throws RemoteException;
+    int getRemainingCapacity() throws RemoteException;
 }
